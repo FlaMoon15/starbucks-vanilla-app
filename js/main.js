8601557c1c8f37612a0ef09c35276cc0ca4efc33
FlaMoon15 <flamoon1403@gmail.com>
2025-08-24 14:37:12 +0900

    badge 추가 및 라이브러리(lodash, gsap) 추가

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+/* sub-menu Event START */
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-/* sub-menu Event START */
 searchEl.addEventListener('click', () => {
   searchInputEl.focus();
 });
@@ -16,3 +16,30 @@ searchInputEl.addEventListener('blur', () => {
   searchInputEl.setAttribute('placeholder', '');
 });
 /* sub-menu Event END */
+
+
+/* BADGE EVENT START */
+const badgeEl = document.querySelector('header .badges');
+
+/**
+ * throttle은 lodash.js 사용
+ * throttle(함수, 시간)
+ * GSAP 사용
+ */
+window.addEventListener('scroll', _.throttle(() => {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) { // Badge Hidden
+    // gsap.to(요소, 지속시간, 옵션)
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none',
+    });
+  } else { // Badge Visible
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block',
+    });
+  }
+}, 300));
+
+/* BADGE EVENT END */
