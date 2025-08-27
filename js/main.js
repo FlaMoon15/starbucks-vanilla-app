203cfac649094feb6d02836522bc8d66a746c7ec
FlaMoon15 <flamoon1403@gmail.com>
2025-08-27 22:29:48 +0900

    common module

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-/* sub-menu Event START */
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', () => {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합 검색');
-});
-
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-/* sub-menu Event END */
-
-
 /* BADGE EVENT START */
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -174,8 +154,3 @@ spyEls.forEach((spyEl) => {
   .addTo(new ScrollMagic.Controller());
 });
 /* ScrollMagic END */
-
-
-/* 연도 계산 */
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
